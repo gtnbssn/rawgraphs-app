f59d2d96a0bbc40081b692883517fc369f525dca
Jason-Ng00 <e0407783@u.nus.edu>
2021-06-22 12:53:46 +0800

    fix warning and css overflow

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,21 +15,21 @@ export default function Footer(props) {
       <Col xs={4} sm={4} lg={4} className="d-none d-sm-none d-lg-block">
         <h2>Resource Librarians</h2>
           <ListGroup className={styles.footer_listItem}>
-            <li> <a href="http://libguides.nus.edu.sg/areastudies" target="_blank">Area Studies</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/business" target="_blank">Business</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/sde" target="_blank">Design &amp; Environment</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32292" target="_blank">Reference Management</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/engineering" target="_blank">Engineering &amp; Computer Science</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/humanities" target="_blank">Humanities</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32284" target="_blank">Law</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32285" target="_blank">Medicine, Pharmacy, &amp; Nursing</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32286" target="_blank">Music</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32293" target="_blank">Patents</a></li>
-            <li> <a href="https://libguides.nus.edu.sg/rdm" target="_blank">Research Data Management</a></li>
-            <li> <a href="https://libguides.nus.edu.sg/researchimpact " target="_blank">Research Impact Measurement</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/science" target="_blank">Science</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/socialscience" target="_blank">Social Sciences</a></li>
-            <li> <a href="http://libguides.nus.edu.sg/yale-nus" target="_blank">Yale-NUS College</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/areastudies" target="_blank" rel="noopener noreferrer">Area Studies</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/business" target="_blank" rel="noopener noreferrer">Business</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/sde" target="_blank" rel="noopener noreferrer">Design &amp; Environment</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32292" target="_blank" rel="noopener noreferrer">Reference Management</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/engineering" target="_blank" rel="noopener noreferrer">Engineering &amp; Computer Science</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/humanities" target="_blank" rel="noopener noreferrer">Humanities</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32284" target="_blank" rel="noopener noreferrer">Law</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32285" target="_blank" rel="noopener noreferrer">Medicine, Pharmacy, &amp; Nursing</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32286" target="_blank" rel="noopener noreferrer">Music</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/sb.php?subject_id=32293" target="_blank" rel="noopener noreferrer">Patents</a></li>
+            <li> <a href="https://libguides.nus.edu.sg/rdm" target="_blank" rel="noopener noreferrer">Research Data Management</a></li>
+            <li> <a href="https://libguides.nus.edu.sg/researchimpact " target="_blank" rel="noopener noreferrer">Research Impact Measurement</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/science" target="_blank" rel="noopener noreferrer">Science</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/socialscience" target="_blank" rel="noopener noreferrer">Social Sciences</a></li>
+            <li> <a href="http://libguides.nus.edu.sg/yale-nus" target="_blank" rel="noopener noreferrer">Yale-NUS College</a></li>
           </ListGroup>
       </Col>
 
@@ -45,7 +45,7 @@ export default function Footer(props) {
             <li> <a href="/frontend/ms/medical-library/about-medical-library" target="_self">Medical Library</a></li>
             <li> <a href="/frontend/ms/music-library/about-music-library" target="_self">Music Library</a></li>
             <li> <a href="/frontend/ms/science-library/about-science-library" target="_self">Science Library</a></li>
-            <li> <a href="http://library.yale-nus.edu.sg" target="_blank">Yale-NUS College Library</a></li>
+            <li> <a href="http://library.yale-nus.edu.sg" target="_blank" rel="noopener noreferrer">Yale-NUS College Library</a></li>
           </ListGroup>
       </Col>
 
@@ -60,26 +60,26 @@ export default function Footer(props) {
               12 Kent Ridge Crescent{'\n'}
               Singapore 119275</pre>
               </li>
-            <li><FontAwesomeIcon icon={faPhoneAlt} className={styles.icon}/><span style={{whiteSpace: 'pre-wrap'}}>+65 6516 2028</span></li>
+            <li><FontAwesomeIcon icon={faPhoneAlt} className={styles.icon}/><span>+65 6516 2028</span></li>
             <li><FontAwesomeIcon icon={faEnvelope} className={styles.icon}/>
-              <span id="cloaked12"><a href="mailto:askalib@nus.edu.sg">askalib@nus.edu.sg</a><br /><a href="mailto:" /></span>
+              <span><a href="mailto:askalib@nus.edu.sg" target="_self">askalib@nus.edu.sg</a><br /></span>
             </li>
           </ListGroup> 
         </Row>
 
         <Row className={styles.footerMinContent}>
           <Nav>
-            <Nav.Link href="https://www.facebook.com/NUSLibraries" target="_blank" className={styles.img}><img id="j_id510:0:j_id512" src="facebook" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://www.facebook.com/NUSLibraries" target="_blank" className={styles.img}><img src="facebook" style={{height: 32, width: 32}} alt="facebook" />
             </Nav.Link>
-            <Nav.Link href="https://twitter.com/NUSlibraries" target="_blank" className={styles.img}><img id="j_id510:1:j_id512" src="twitter" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://twitter.com/NUSlibraries" target="_blank" className={styles.img}><img src="twitter" style={{height: 32, width: 32}} alt="twitter" />
             </Nav.Link>
-            <Nav.Link href="https://instagram.com/nuslibraries" target="_blank" className={styles.img}><img id="j_id510:2:j_id512" src="instagram" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://instagram.com/nuslibraries" target="_blank" className={styles.img}><img src="instagram" style={{height: 32, width: 32}} alt="instagram" />
             </Nav.Link>
-            <Nav.Link href="http://blog.nus.edu.sg/linus/" target="_blank" className={styles.img}><img id="j_id510:3:j_id512" src="word" style={{height: 32, width: 32}} />
+            <Nav.Link href="http://blog.nus.edu.sg/linus/" target="_blank" className={styles.img}><img src="word" style={{height: 32, width: 32}} alt="word" />
             </Nav.Link>
-            <Nav.Link href="https://www.youtube.com/user/nuslibraries" target="_blank" className={styles.img}><img id="j_id510:4:j_id512" src="youtube" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://www.youtube.com/user/nuslibraries" target="_blank" className={styles.img}><img src="youtube" style={{height: 32, width: 32}} alt="youtube" />
             </Nav.Link>
-            <Nav.Link href="https://www.flickr.com/photos/nuslibraries" target="_blank" className={styles.img}><img id="j_id510:5:j_id512" src="flickr" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://www.flickr.com/photos/nuslibraries" target="_blank" className={styles.img}><img src="flickr" style={{height: 32, width: 32}} alt="flickr" />
             </Nav.Link>
           </Nav>
         </Row>
@@ -97,24 +97,24 @@ export default function Footer(props) {
               </li>
             <li><FontAwesomeIcon icon={faPhoneAlt} className={styles.icon}/><span style={{whiteSpace: 'pre-wrap'}}>+65 6516 2028</span></li>
             <li><FontAwesomeIcon icon={faEnvelope} className={styles.icon}/>
-              <span><a href="mailto:askalib@nus.edu.sg">askalib@nus.edu.sg</a><br /><a href="mailto:" /></span>
+              <span><a href="mailto:askalib@nus.edu.sg">askalib@nus.edu.sg</a><br /></span>
             </li>
           </ListGroup> 
         </Row>
 
         <Row className={styles.footerMinContent}>
           <Nav>
-            <Nav.Link href="https://www.facebook.com/NUSLibraries" target="_blank" className={styles.img}><img id="j_id510:0:j_id512" src="facebook" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://www.facebook.com/NUSLibraries" target="_blank" className={styles.img}><img src="facebook" style={{height: 32, width: 32}} alt="facebook" />
             </Nav.Link>
-            <Nav.Link href="https://twitter.com/NUSlibraries" target="_blank" className={styles.img}><img id="j_id510:1:j_id512" src="twitter" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://twitter.com/NUSlibraries" target="_blank" className={styles.img}><img src="twitter" style={{height: 32, width: 32}} alt="twitter" />
             </Nav.Link>
-            <Nav.Link href="https://instagram.com/nuslibraries" target="_blank" className={styles.img}><img id="j_id510:2:j_id512" src="instagram" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://instagram.com/nuslibraries" target="_blank" className={styles.img}><img src="instagram" style={{height: 32, width: 32}} alt="instagram" />
             </Nav.Link>
-            <Nav.Link href="http://blog.nus.edu.sg/linus/" target="_blank" className={styles.img}><img id="j_id510:3:j_id512" src="word" style={{height: 32, width: 32}} />
+            <Nav.Link href="http://blog.nus.edu.sg/linus/" target="_blank" className={styles.img}><img src="word" style={{height: 32, width: 32}} alt="word" />
             </Nav.Link>
-            <Nav.Link href="https://www.youtube.com/user/nuslibraries" target="_blank" className={styles.img}><img id="j_id510:4:j_id512" src="youtube" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://www.youtube.com/user/nuslibraries" target="_blank" className={styles.img}><img src="youtube" style={{height: 32, width: 32}} alt="youtube" />
             </Nav.Link>
-            <Nav.Link href="https://www.flickr.com/photos/nuslibraries" target="_blank" className={styles.img}><img id="j_id510:5:j_id512" src="flickr" style={{height: 32, width: 32}} />
+            <Nav.Link href="https://www.flickr.com/photos/nuslibraries" target="_blank" className={styles.img}><img src="flickr" style={{height: 32, width: 32}} alt="flickr" />
             </Nav.Link>
           </Nav>
         </Row>
@@ -132,10 +132,10 @@ export default function Footer(props) {
         </Row>
         <Row className={styles.footerBottom}>
           <Nav>
-              <li><a href="http://www.nus.edu.sg/legal-information-notices" target="_blank">Legal</a></li><FontAwesomeIcon icon={faCircle} className={styles.pseudoIcon}/>
-              <li><a href="http://www.nus.edu.sg/identity" target="_blank">Branding guidelines</a></li><FontAwesomeIcon icon={faCircle} className={styles.pseudoIcon}/>
-              <li><a href="http://www.nus.edu.sg/contact" target="_blank">Contact us</a></li><FontAwesomeIcon icon={faCircle} className={styles.pseudoIcon}/>
-              <li><a href="http://cn.nus.edu.sg/" target="_blank">中文</a></li>
+              <li><a href="http://www.nus.edu.sg/legal-information-notices" target="_blank" rel="noopener noreferrer">Legal</a></li><FontAwesomeIcon icon={faCircle} className={styles.pseudoIcon}/>
+              <li><a href="http://www.nus.edu.sg/identity" target="_blank" rel="noopener noreferrer">Branding guidelines</a></li><FontAwesomeIcon icon={faCircle} className={styles.pseudoIcon}/>
+              <li><a href="http://www.nus.edu.sg/contact" target="_blank" rel="noopener noreferrer">Contact us</a></li><FontAwesomeIcon icon={faCircle} className={styles.pseudoIcon}/>
+              <li><a href="http://cn.nus.edu.sg/" target="_blank" rel="noopener noreferrer">中文</a></li>
           </Nav>
         </Row>
       </Col>
